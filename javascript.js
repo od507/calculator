a43ca3ece3a36bbc92eb01494c281a7736654f59
Antonio Daniels <68254453+od507@users.noreply.github.com>
2022-08-19 16:55:02 -0500

    added initial files

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function add(numberOne,numberTwo){
+    return numberOne + numberTwo
+}
+
+function subtract(numberOne,numberTwo){
+    return numberOne-numberTwo
+}
+
+function multiply(numberOne,numberTwo){
+    return numberOne*numberTwo
+}
+
+function divide(numberOne,numberOne){
+    return numberOne/numberTwo
+}
+
+
